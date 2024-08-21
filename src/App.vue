fe0f1fce988bbedc2f498c75821a119f8bf90dcc
zk202308A <zk202308A@gmail.com>
2024-08-21 10:39:23 +0900

    0821

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CountContainer from './components/ex1/CountContainer.vue';
+</script>
+
+<template>
+  <CountContainer></CountContainer>
+</template>
+
+<style scoped>
+
+</style>
