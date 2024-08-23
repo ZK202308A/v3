e15b8b3c71aaf7b233348737af024012ec59bcd6
zk202308A <zk202308A@gmail.com>
2024-08-23 15:43:48 +0900

    COMMIT0823

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <script setup>
 import { RouterView, RouterLink } from 'vue-router';
 import useMember from './stores/useMember';
-import { computed, watch } from 'vue';
 
-const {member, getMid, logout} = useMember()
+const {member, logout} = useMember()
 
-
-const mid = computed(() => member.value?.mid)
 
 
 </script>
 
 <template>
-  {{ member }}
   <div>
     <span> <RouterLink to="/">Main</RouterLink>  </span>
     <span> <RouterLink to="/about">About</RouterLink>  </span>
     <span> <RouterLink to="/todo">Todo</RouterLink>  </span>
-    <span v-if="mid === null"> <RouterLink to="/login">Login</RouterLink> </span>
+    
+    <span v-if="member.mid === null"> <RouterLink to="/login">Login</RouterLink> </span>
 
-    <span v-if="mid !== null"> <button @click="logout" >Logout</button> </span>
+    <span v-if="member.mid  !== null"> <button @click="logout" >Logout</button> </span>
 
   </div>
 
-  <div v-if="mid !== null">
+  <div >
      
     <hr/>
-      MemberID : {{ mid }}   
+      MemberID : {{ member.value?.mid  }}   
     <hr/>
   </div>
 
--- src/stores/useMember.js
+++ src/stores/useMember.js
@@ -6,8 +6,10 @@ const useMember = defineStore('memberStore', () => {
   const member = ref( {mid: null} )
 
   const login = (value) => {
+    
   
     console.log("login..." + value)
+
     member.value.mid = value
 
     localStorage.setItem('mid', value)
@@ -16,37 +18,12 @@ const useMember = defineStore('memberStore', () => {
 
   const logout = () => {
 
-    console.log("logout") 
     member.value.mid = null
 
     localStorage.removeItem('mid')
   }
 
-  const getMid = computed( () => {
-
-    console.log("1")
-
-    if( ! localStorage.getItem('mid')) {
-      return null
-    }
-
-    console.log("2")
-    if( localStorage.getItem("mid")  && ! member.value.mid ) {
-      member.value.mid = localStorage.getItem("mid")
-    }
-    console.log("3")
-    if(member.value.mid){
-      return member.value.mid
-    }
-    console.log("4")
-
-    return localStorage.getItem("mid")
-
-  })
-
-  const memberId = computed(() => member.value.mid)
-
-  return {login, logout, memberId, member,  getMid }
+  return {login, logout, member }
 
 })
 
